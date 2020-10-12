89428fa8b885dce720deda9ac815502ec9aae2fa
Mittaus Cruz <EXPERIS-Isaias.Mayon@spsa.pe>
2020-10-12 15:34:49 -0500

    fix: Se configura conexión a base de datos

--- main.go
+++ main.go
@@ -4,16 +4,23 @@ import (
 	"log"
 
 	application "dev.azure.com/spsa/wspromo/application"
-	"dev.azure.com/spsa/wspromo/config"
+	config "dev.azure.com/spsa/wspromo/config"
 	server_routes "dev.azure.com/spsa/wspromo/infraestructure/gin.server"
 	v1 "dev.azure.com/spsa/wspromo/infraestructure/gin.server/v1"
 	repository "dev.azure.com/spsa/wspromo/infraestructure/repository"
+	"github.com/jinzhu/configor"
 	"github.com/jinzhu/gorm"
 )
 
 func main() {
 	var dbs []*gorm.DB
 	//inicializar base de datos
+	configDb := &config.GORMConfig{}
+	err := configor.Load(configDb, "config.yml")
+	if err != nil {
+		panic("No se encontró archivo de configuración")
+	}
+	dbs, _ = repository.NewDb(configDb)
 	repository := repository.NewDatacenterRepository(dbs)
 	app := application.NewDatacenter(repository)
 	datacenterHandler := v1.NewDatacenterHandler(*app)
